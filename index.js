6f0015a9d6be055a5aab512c55c25b7e69e8907e
capture-grey <capturegrey@gmail.com>
2025-09-13 09:59:08 +0600

    add:vercel.json

--- index.js
+++ index.js
@@ -2,36 +2,32 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-const path = require("path");
 const cookieParser = require("cookie-parser");
+const serverless = require("serverless-http");
 
-//_____________________________________________________________________internal imports________________________________________________________________________
+// internal imports
 const {
   notFoundHandler,
   errorHandler,
 } = require("./middlewares/error.middleware");
 const connectDB = require("./config/db.js");
 
-//______________________________Routers__________________________________
-
 const authRouter = require("./router/authRouter");
 const roomRouter = require("./router/roomRouter");
 const bookingRouter = require("./router/bookingRouter");
 
-//create express app
+// create express app
 const app = express();
 
-//process.env
+// env
 dotenv.config();
-
-// database connection
 connectDB();
 
-// request parsers
+// parsers
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//___________________________________________________________________API Routes_____________________________________________________________________________________
+// routes
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello From Octobill" });
 });
@@ -40,14 +36,14 @@ app.use("/api/auth", authRouter);
 app.use("/api/rooms", roomRouter);
 app.use("/api/bookings", bookingRouter);
 
-// 404 not found handler
+// error handlers
 app.use(notFoundHandler);
-
-// common error handler
 app.use(errorHandler);
+
+// export for vercel
+module.exports = app;
+module.exports.handler = serverless(app);
 
 // app.listen(process.env.PORT, () => {
 //   console.log(`app listening to port ${process.env.PORT}`);
 // });
-
-module.exports = app;
